9ad3c61da3218acd877d9aca55649572e179c1da
mann-prajapati05 <mannprajapati0284@gmail.com>
2026-05-05 11:29:30 +0530

    fix minio and postgres for dev

--- client/src/Auth.jsx
+++ client/src/Auth.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:4041";
+const API_BASE = import.meta.env.VITE_API_BASE || "http://backend:5000";
 
 export default function Auth({ initialMode = "login" }) {
   const navigate = useNavigate();
--- server/app.js
+++ server/app.js
@@ -37,7 +37,7 @@ app.use('/', (req, res, next) => {
 app.use('/auth', authRouter);
 app.use('/notes', noteRouter);
 
-const PORT=process.env.PORT || 4041;
+const PORT=process.env.PORT || 5000;
 app.listen(PORT,"0.0.0.0",()=>{
     console.log(`Running on http://localhost:${PORT}`);
 });
